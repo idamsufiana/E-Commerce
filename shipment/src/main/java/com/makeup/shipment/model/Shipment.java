c76fd3c5b042c78840cf102a073799823520a721
ida.sufiana <ida.sufiana@consultant.ioh.co.id>
2026-01-11 22:31:38 +0700

    fix pom

--- shipment/src/main/java/com/makeup/shipment/model/Shipment.java
+++ shipment/src/main/java/com/makeup/shipment/model/Shipment.java
@@ -1,5 +1,18 @@
 package com.makeup.shipment.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "shipments")
 @Data
