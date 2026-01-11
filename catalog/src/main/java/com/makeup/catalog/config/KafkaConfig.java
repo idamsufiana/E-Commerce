ba22371965cb77ac5c5d584e5219a42c925edbfe
ida.sufiana <ida.sufiana@consultant.ioh.co.id>
2026-01-11 22:09:39 +0700

    bug fix

--- catalog/src/main/java/com/makeup/catalog/config/KafkaConfig.java
+++ catalog/src/main/java/com/makeup/catalog/config/KafkaConfig.java
@@ -46,7 +46,7 @@ public class KafkaConfig {
         Map<String, Object> props = new HashMap<>();
 
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "billing-service");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "catalog-service");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
