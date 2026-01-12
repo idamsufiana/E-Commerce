4c142b36527ea8a88760f688ec29b080fa1ab9d3
ida.sufiana <ida.sufiana@consultant.ioh.co.id>
2026-01-12 13:24:11 +0700

    add callback

--- payment/src/main/java/com/makeup/payment/controller/PaymentController.java
+++ payment/src/main/java/com/makeup/payment/controller/PaymentController.java
@@ -1,4 +1,30 @@
 package com.makeup.payment.controller;
 
+import com.makeup.payment.dto.PaymentWebhookRequest;
+import com.makeup.payment.service.PaymentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payments/webhook")
 public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> callback(
+            @RequestBody PaymentWebhookRequest payload
+    ) {
+        if ("SUCCESS".equals(payload.getStatus())) {
+            paymentService.markSuccess(payload.getOrderId());
+        }
+        return ResponseEntity.ok().build();
+    }
 }
---
+++ payment/src/main/java/com/makeup/payment/dto/PaymentWebhookRequest.java
@@ -0,0 +1,9 @@
+package com.makeup.payment.dto;
+
+import lombok.Data;
+
+@Data
+public class PaymentWebhookRequest {
+    private Long orderId;
+    private String status;
+}
